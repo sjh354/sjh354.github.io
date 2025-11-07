57bfa20da1f3204728c813751c54bc4cafc5e7fc
sjh354 <102205688+sjh354@users.noreply.github.com>
2025-11-07 14:36:25 +0000

    Deploying to gh-pages from @ sjh354/sjh354.github.io@f087ca223ea1deae025a03588aca508ec02a23e1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,62 +30,22 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-페이지-개설",
+        },{id: "news-페이지-개설",
           title: '페이지 개설!!!!!',
           description: "",
           section: "News",},{id: "news-이-페이지-주인장-전역",
           title: '🎉 이 페이지 주인장 전역 🎉',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-markdown-editor",
+          title: 'Markdown Editor',
+          description: "online markdown editor",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/markdown_editor/";
+            },},{id: "projects-minesweeper",
+          title: 'minesweeper',
+          description: "back to the future",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/minesweeper/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
